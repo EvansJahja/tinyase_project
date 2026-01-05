11136adaf31055219a43cea6e41d4bc0ac127a26
Evans Jahja <evansjahja13@gmail.com>
2026-01-05 09:59:56 +0900

    switching to zerocopy....

--- tinyase/src/parser.rs
+++ tinyase/src/parser.rs
@@ -1,5 +1,5 @@
 
-use nom::number::complete::{le_u16};
+use nom::{bytes::complete::take, combinator::{self, rest, success, value}, multi::{length_data, length_value}, number::complete::{le_u16, le_u32}};
 
 use nom_derive::*;
 use alloc::vec::Vec;
@@ -8,7 +8,7 @@ use alloc::vec::Vec;
 #[derive(Debug)]
 #[derive(NomLE)]
 pub struct ASEHeader<'a> {
-    pub header: u32,
+    pub filesize: u32,
     #[nom(Tag(b"\xE0\xA5"))]
     _magic: &'a[u8],
     pub frames: u16,
@@ -17,7 +17,8 @@ pub struct ASEHeader<'a> {
     pub depth: u16,
     pub flags: u32,
     _deprecated_speed: u16,
-    _reserved: u16,
+    _reserved_11: u32,
+    _reserved_12: u32,
     pub pallet_transparent_idx: u8,
     _reserved2: [u8; 3],
     pub num_colors: u16,
@@ -60,24 +61,70 @@ pub struct ASEHeader<'a> {
 // }
 
 #[derive(NomLE, Debug)]
-struct ASE<'a> {
-    header: ASEHeader<'a>,
-    #[nom(Count="header.frames")]
-    frames: Vec<ASEFrame>,
+pub struct ASE<'a> {
+    pub header: ASEHeader<'a>,
+    #[nom(Count="header.frames as usize")]
+    pub frames: Vec<ASEFrame<'a>>,
+}
+
+#[derive(NomLE, Debug)]
+pub struct ASEFrame<'a> {
+    num_bytes: u32,
+    #[nom(Tag(b"\xfa\xf1"))]
+    _magic: &'a[u8],
+    old_unused: u16,
+    duration: u16,
+    _reserved: [u8; 2],
+    num_chunks: u32,
+    // #[nom(Count="num_chunks")]
+    // pub chunks: Vec<ASEChunk<'a>>,
+    pub chunks: [ASEChunk<'a>; 200],
+}
+
+#[derive(NomLE, Debug)]
+pub struct ASEChunk<'a> {
+    pub size: u32,
+    pub chunk_type: u16,
+    // #[nom(Count="size as usize - 6")]
+    // data: Vec<u8>
+    #[nom(Parse = "(|i| parse_chunk_data(i, chunk_type, size))")]  
+    chunk_data: ChunkData<'a>,
 
 }
-#[derive(NomLE, Debug)]
-struct ASEFrame {
-    duration: u16,
-    // chunks: Vec<ASEChunk<'a>>,
+
+#[derive(Debug)]
+enum ChunkData<'a> {
+    Unknown(&'a[u8]),
 }
+
+
+fn parse_chunk_data(input: &[u8], chunk_type: u16, size: u32) -> nom::IResult<&[u8], ChunkData> {
+    match chunk_type {
+        _ => {
+            // let (input, data) = combinator::rest(input)?;
+            // Ok((input, ChunkData::Unknown(data.to_vec())))
+            // take (size as usize - 6usize)(input)
+            let (input, data) = take(size as usize - 6usize)(input)?;
+            Ok((input, ChunkData::Unknown(data)))
+        }
+    }
+}
+
 
 
 pub fn parse_header(input: &'_ [u8]) -> nom::IResult<&'_ [u8], ASEHeader<'_>> {
     ASEHeader::parse(input)
 }
 
-fn parse_aseprite(input: &[u8]) -> nom::IResult<&[u8], ASE> {
+pub fn parse_aseprite(input: &[u8]) -> nom::IResult<&[u8], ASE> {
+    let mut a = (
+        ASEHeader::parse,
+    );
+    let b = a.parse(input);
+
+
+
+
     ASE::parse(input)
 }
 
@@ -85,6 +132,7 @@ fn parse_aseprite(input: &[u8]) -> nom::IResult<&[u8], ASE> {
 #[cfg(test)]
 mod test {
     use compression::prelude::{EncodeExt, ZlibEncoder};
+    use nom::error::dbg_dmp;
 
     use super::*;
     // #[test]
@@ -99,8 +147,8 @@ mod test {
     fn test_read() {
         let a = std::fs::read("tests/anim_idle.ase").unwrap();
 
-        let (_, ase) = ASE::parse(&a).unwrap();
-        println!("{:?}", ase);
+        let (rest, ase) = ASE::parse(&a).unwrap();
+        println!("{:#x?}, rest: {}", ase, rest.len());
 
 
     }
