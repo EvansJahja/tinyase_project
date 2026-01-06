a00dad59afc38cfb00c6d61dd5c1eaa9e95fed73
Evans Jahja <evansjahja13@gmail.com>
2026-01-07 01:03:36 +0900

    use get() insead of implementing Deref

--- tinyase/src/parser.rs
+++ tinyase/src/parser.rs
@@ -1,13 +1,6 @@
-
-// use nom::{bytes::complete::take, combinator::{self, rest, success, value}, multi::{length_data, length_value}, number::complete::{le_u16, le_u32}};
-
-// use nom_derive::*;
 use alloc::vec::Vec;
 use thiserror::Error;
-// use zerocopy_derive::*;  
-// use zerocopy::{FromBytes, KnownLayout, Immutable};  
 use zerocopy::*;
-// use nom::{IResult, Parser, bytes::complete::{tag, take}, number::complete::{le_u16, le_u32, u8} };
 use core::convert::Infallible;
 use core::iter::Iterator;
 use core::ops::Index;
@@ -39,42 +32,6 @@ pub struct ASEHeader {
     _reserved3: [u8; 84],
 }
 
-// fn parse_header(input: &[u8]) -> IResult<&[u8], MyAwesomeStruct> {
-//     let (input, 
-//             ( header
-//             , _magic
-//             , frames
-//             , width
-//             , height
-//             , depth
-//             , flags
-//         )) =
-//         ( le_u32
-//         , tag(&b"\xE0\xA5"[..])
-//         , le_u16
-//         , le_u16
-//         , le_u16
-//         , le_u16
-//         , le_u32
-//     ).parse_complete(input)?;
-
-//     Ok((input, MyAwesomeStruct {
-//         header: header,
-//         frames: frames,
-//         width: width,
-//         height: height,
-//         depth: depth,
-//         flags: flags,
-//     }))
-// }
-
-// #[derive(NomLE, Debug)]
-// pub struct ASE<'a> {
-//     pub header: ASEHeader<'a>,
-//     #[nom(Count="header.frames as usize")]
-//     pub frames: Vec<ASEFrame<'a>>,
-// }
-
 // #[derive(NomLE, Debug)]
 #[derive(Debug, FromBytes, KnownLayout, Immutable)]
 #[repr(packed)]
@@ -100,35 +57,6 @@ impl<'a> IntoIterator for ASEFrameContainer<'a> {
         }
     }
 }
-
-// #[derive(NomLE, Debug)]
-// pub struct ASEChunk<'a> {
-//     pub size: u32,
-//     pub chunk_type: u16,
-//     // #[nom(Count="size as usize - 6")]
-//     // data: Vec<u8>
-//     #[nom(Parse = "(|i| parse_chunk_data(i, chunk_type, size))")]  
-//     chunk_data: ChunkData<'a>,
-
-// }
-
-// #[derive(Debug)]
-// enum ChunkData<'a> {
-//     Unknown(&'a[u8]),
-// }
-
-
-// fn parse_chunk_data(input: &[u8], chunk_type: u16, size: u32) -> nom::IResult<&[u8], ChunkData> {
-//     match chunk_type {
-//         _ => {
-//             // let (input, data) = combinator::rest(input)?;
-//             // Ok((input, ChunkData::Unknown(data.to_vec())))
-//             // take (size as usize - 6usize)(input)
-//             let (input, data) = take(size as usize - 6usize)(input)?;
-//             Ok((input, ChunkData::Unknown(data)))
-//         }
-//     }
-// }
 
 #[derive(Error, Debug)]
 pub enum HeaderParseError {
@@ -185,27 +113,12 @@ pub trait NextResult<'a> {
     fn next(&'a self) -> Result<(Self::Output, &'a [u8]), Self::Error>;
 }
 
-// impl<'a> NextResult<'a> for Result<(&'a ASEChunkHeader, &'a [u8]), ChunkHeaderParseError> {
-//     type Output = &'a ASEChunkHeader;
-//     type Error = ChunkHeaderParseError;
-//     // We need to check current size, and return a new result with updated slice
-//     fn next(&'a self) -> Result<(Self::Output, &'a [u8]), Self::Error> {
-//         match self {
-//             Ok((chunk, rest)) => {
-//                 let ofs : usize = chunk.size() - 6;
-//                 let next_chunk_start = &rest[ofs..];
-//                 Self::parse_chunk_header(next_chunk_start)
-//             },
-//             Err(e) => Err(e.clone()),
-//         }
-//     }
-// }
-
 trait HasSize {
     fn size(&self) -> usize;
 }
 
 
+// ptr Points to start of chunk header, which is <u32 size><u16 type>
 #[derive(Debug, Clone)]
 pub struct ChunkPtr<'a> {
     ptr: &'a [u8],
@@ -217,7 +130,7 @@ impl ChunkPtr<'_> {
         self.count
     }
 
-    fn get<'a>(&'a self, index: usize) -> Option<ChunkPtr<'a>> {
+    fn get<'a>(&'a self, index: usize) -> Option<(&'a ASEChunkHeader, &'a [u8])> {
         let mut w = self.clone();
         // let mut w: ChunkPtr<'a>  = self.clone();
         if index >= self.count {
@@ -228,13 +141,10 @@ impl ChunkPtr<'_> {
                 panic!("Index out of bounds");
             }
         }
-        w.next()
 
-
-        // w.next().map(|(a ,b)| {ChunkPtr {
-        //     ptr: b,
-        //     count: a.size as usize,
-        // }})
+        w.next().map(|chunk_ptr|{
+            ASEChunkHeader::ref_from_prefix(chunk_ptr.ptr).ok()
+        }).flatten()
 
     }
 }
@@ -250,37 +160,7 @@ struct ChunkCel {
     cel_type: u16,
     z_index: i16,
     _reserved: [u8; 5],
-
-
 }
-
-
-
-    
-impl<'a> Deref for ChunkPtr<'a> {
-    type Target = ASEChunkHeader;
-    
-    fn deref (&self) -> &'a Self::Target {
-        const SIZE: usize = 4;
-        let chunk_ptr =&self.ptr[SIZE..];
-        let (chunk_header, rest) = ASEChunkHeader::ref_from_prefix(&chunk_ptr).unwrap();
-        ChunkCel::
-        
-
-        // UnparsedChunk::r
-    }
-}
-
-
-// impl<'a> Mwahaha<'a, &'a ASEChunkHeader, ChunkHeaderParseError> for ChunkPtr<'a, &'a ASEChunkHeader, ChunkHeaderParseError> {
-//     fn parse_chunk_header(input: &'a [u8]) -> Result<(&'a ASEChunkHeader, &'a [u8]), ChunkHeaderParseError> {
-//         let (chunk, rest) = ASEChunkHeader::ref_from_prefix(&input)
-//             .map_err(|_| ChunkHeaderParseError::CastError)?;
-
-//         Ok((chunk, rest))
-//     }
-// }
-    
 
 impl<'a> Iterator for ChunkPtr<'a> 
 {
@@ -323,16 +203,7 @@ impl<'a> Iterator for ChunkPtr<'a>
 
 #[cfg(test)]
 mod test {
-    // use compression::prelude::{EncodeExt, ZlibEncoder};
-
     use super::*;
-    // #[test]
-    // fn test_parse_u32() {
-    //     let data = [0x12, 0x34, 0x56, 0x78, 0x9A];
-    //     let (remaining, value) = parse_header(&data).unwrap();
-
-    //     println!("{:?}", value);
-    // }
 
     #[test]
     fn test_read() {
@@ -351,9 +222,10 @@ mod test {
         // let (rest, ase) = ASE::parse(&a).unwrap();
         // println!("{:#x?}, rest: {}", ase, rest.len());
         // let w = (c[0]);
-        let w = c.get(2).unwrap();
-        let x = &w[0..10];
-        println!("{:#x?}", w.chunk);
+        let (w, rest) = c.get(17).unwrap();
+        let x = w;
+        let ct = w.chunk_type;
+        println!("{:#x?}", ct);
 
 
     }
